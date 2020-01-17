f7264c4ec9d7265c446668ff8be4286f9c7368c1
Amuzu Oluwapelumi <35280873+THErealARETE@users.noreply.github.com>
2020-01-17 17:26:30 +0100

    changes to myscrumy/myscrumy

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -74,14 +74,19 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER' : 'root',
+        'PASSWORD': 'gerrard134',
+
 }
 
+}
+# manage.py migrate --run-syncdb
 
-# Password validation
+
+# Password validation   
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
